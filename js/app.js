7f53bfb8c2707c29f2f5201e1890434b8a3c3db0
Tako <matiasrain73@gmail.com>
2025-07-03 01:14:14 -0300

    Actualizando
    Que el crear cuenta e iniciar sesion, prosigan sin necesidad de correo y contraseña especifica

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 document.getElementById("loginButton").addEventListener("click", () => {
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const error = document.getElementById("error-message");
 
-  if (username === "admin" && password === "1234") {
-    window.location.href = "loading.html";
-  } else {
-    error.textContent = "Usuario o contraseña incorrectos.";
-  }
+  window.location.href = "loading.html";
 });
 
 document.getElementById("skipButton").addEventListener("click", () => {
@@ -18,3 +11,4 @@ document.getElementById("togglePassword").addEventListener("click", () => {
   const passwordInput = document.getElementById("password");
   passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 });
+
